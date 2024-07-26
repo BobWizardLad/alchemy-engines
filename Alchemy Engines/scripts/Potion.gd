6e4cb54ee9eb8025a9428013100689b13ab8fdcb
BobWizardLad <bobwizardlad@Gmail.com>
2024-07-26 11:37:33 -0500

    :sparkles: Potion Template

--- Alchemy Engines/scripts/Potion.gd
+++ Alchemy Engines/scripts/Potion.gd
@@ -2,21 +2,21 @@ extends Resource
 class_name Potion
 
 # Potion stat layout for fuel burning
-# armor
-# damage
-# resistance
-# special_damage
-# initiative
+@export var armor_stat: float # armor
+@export var damage_stat: int # damage
+@export var resistance_stat: float # resistance
+@export var special_stat: int # special_damage
+@export var initiative_stat: int # initiative
 
 # Potion stats for city burn
-# burn_duration
-# farming_yield
+@export var city_burn_duration: int # burn_duration
+@export var farming_yield: int # farming_yield
 
 # Potion effects for thrown tile effects
-# is_dot - bool
-# damage_over_time - int
-# dot_duration - int
-# engage_damage - int
-# will_glow - bool
-# tile_duration - int
-# spread - int
+@export var is_dot: bool # is_dot - bool
+@export var damage_over_time: int # damage_over_time - int
+@export var dot_duration: int # dot_duration - int
+@export var engage_damage: int # engage_damage - int
+@export var will_glow: bool # will_glow - bool
+@export var tile_duration: int # tile_duration - int
+@export var spread: int # spread - int
